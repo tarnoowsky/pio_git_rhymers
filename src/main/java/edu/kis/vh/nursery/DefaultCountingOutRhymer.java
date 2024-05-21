cbef48800d2f3c4d475fc11b290c4ce8a0a172e3
matmirowski <mateusz.m@niceguys.info>
2024-05-21 21:12:00 +0200

    6. Zastapienie literalu stalymi

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,13 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
 
-    public int total = -1;
+    public static final int ARRAY_SIZE = 12;
+    public static final int ERROR_RESPONSE = -1;
+    public static final int LAST_ARRAY_INDEX = 11;
+    private int[] numbers = new int[ARRAY_SIZE];
+
+    public int total = ERROR_RESPONSE;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +16,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == ERROR_RESPONSE;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == LAST_ARRAY_INDEX;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return ERROR_RESPONSE;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return ERROR_RESPONSE;
         return numbers[total--];
     }
 
--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,7 +2,8 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
-    int totalRejected = 0;
+    public static final int INITIAL_REJECTED_VALUE = 0;
+    int totalRejected = INITIAL_REJECTED_VALUE;
 
     public int reportRejected() {
         return totalRejected;
