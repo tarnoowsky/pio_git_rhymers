93af6889828d0fccb0702afc83583eba1591cd7a
Szymon Tarnowski <szymontarno@gmail.com>
2024-05-26 20:57:58 +0200

    12. Usunięcie nieużywanego gettera

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -13,10 +13,6 @@ public class DefaultCountingOutRhymer {
 
     private int total = DEFAULT_VALUE;
 
-    public int getTotal() {
-        return total;
-    }
-
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
